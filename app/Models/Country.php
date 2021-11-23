b0b087e1e80c37b4188896d7d75458265eca1742
Roman Sidash <sidash.roman@mail.ru>
2021-11-23 14:53:56 +0600

    Add Country model

---
+++ app/Models/Country.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Country extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $dates = [ 'deleted_at' ];
+
+    protected $fillable = [
+        'country_code',
+        'name_en',
+        'name_ru',
+        'phone_code',
+    ];
+
+}
