4a62340ae14ebab64b3fda4504953aeeca309084
Roman Sidash <sidash.roman@mail.ru>
2021-11-23 14:54:53 +0600

    Add create countries migration

---
+++ database/migrations/2021_11_23_080709_create_countries_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCountriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->id();
+            $table->string('country_code', 2)->unique();
+            $table->string('name_en')->unique();
+            $table->string('name_ru')->unique();
+            $table->string('phone_code');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('countries');
+    }
+}
