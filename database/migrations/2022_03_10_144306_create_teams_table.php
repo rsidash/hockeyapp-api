80df0c77e7a82991920bf973262016fe299ded3a
Roman Sidash <sidash.roman@mail.ru>
2022-03-15 18:45:23 +0600

    feat: add team migration

---
+++ database/migrations/2022_03_10_144306_create_teams_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTeamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->text('description')->nullable();
+            $table->unsignedBigInteger('image_id')->nullable();
+            $table->unsignedBigInteger('owner_id');
+            $table->unsignedBigInteger('city_id');
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table
+                ->foreign('image_id')
+                ->references('id')->on('images')
+                ->onDelete('restrict');
+
+            $table
+                ->foreign('owner_id')
+                ->references('id')->on('users')
+                ->onDelete('restrict');
+
+            $table
+                ->foreign('city_id')
+                ->references('id')->on('cities')
+                ->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teams');
+    }
+}
